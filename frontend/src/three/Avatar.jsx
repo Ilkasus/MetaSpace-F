eb98050bddb7ee1f04aa3e16edbf18b00cc516eb
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-05-29 17:48:14 +0500

    Create Avatar.jsx

---
+++ frontend/src/three/Avatar.jsx
@@ -0,0 +1,14 @@
+import { useGLTF } from '@react-three/drei'
+
+export default function Avatar({ position = [0, 0, 0], nickname = "Guest" }) {
+  const { scene } = useGLTF('/models/avatar.glb')
+  return (
+    <group position={position}>
+      <primitive object={scene} scale={0.3} />
+      <mesh position={[0, 2, 0]}>
+        <textGeometry args={[nickname, { size: 0.3, height: 0.05 }]} />
+        <meshBasicMaterial color="white" />
+      </mesh>
+    </group>
+  )
+}
