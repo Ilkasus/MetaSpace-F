93f51763a5b4163016eb05adaf9a05a46e4dc02a
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-06-04 14:47:22 +0500

    Update Avatar.jsx

--- frontend/src/three/Avatar.jsx
+++ frontend/src/three/Avatar.jsx
@@ -1,30 +1,19 @@
-import { Text } from '@react-three/drei'
-import { useRef } from 'react'
+import { useGLTF, extend } from '@react-three/drei'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import * as THREE from 'three'
 
-function PlayerAvatar({ position, rotation, nickname, isSelf }) {
-  const ref = useRef()
+extend({ TextGeometry })
 
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.position.lerp(new THREE.Vector3(...position), 0.2)
-      ref.current.rotation.y = rotation[1]
-    }
-  })
+export default function Avatar({ position = [0, 0, 0], nickname = "Guest" }) {
+  const { scene } = useGLTF('/models/Avatar.glb')
 
   return (
-    <group ref={ref}>
-      <Avatar scale={0.5} />
-      <Text
-        position={[0, 2.2, 0]}
-        fontSize={0.3}
-        height={0.05}
-        color={isSelf ? 'blue' : 'white'}
-        anchorX="center"
-        anchorY="middle"
-      >
-        {nickname}
-      </Text>
+    <group position={position}>
+      <primitive object={scene} scale={0.3} />
+      <mesh position={[0, 2, 0]}>
+        <textGeometry args={[nickname, { size: 0.3, height: 0.05, curveSegments: 12 }]} />
+        <meshBasicMaterial color="white" />
+      </mesh>
     </group>
   )
 }
