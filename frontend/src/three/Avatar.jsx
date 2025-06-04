348817a0d786ecfaba3f6cbea9e15510a91b5725
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-06-04 14:24:56 +0500

    Update Avatar.jsx

--- frontend/src/three/Avatar.jsx
+++ frontend/src/three/Avatar.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from '@react-three/drei'
 
 export default function Avatar({ position = [0, 0, 0], nickname = "Guest" }) {
-  const { scene } = useGLTF('/models/avatar.glb')
+  const { scene } = useGLTF('/models/Avatar.glb')
   return (
     <group position={position}>
       <primitive object={scene} scale={0.3} />
