e80f1b73a3f36938e4376ab45ff7a384238b9356
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-06-04 14:38:29 +0500

    Update Avatar.jsx

--- frontend/src/three/Avatar.jsx
+++ frontend/src/three/Avatar.jsx
@@ -1,12 +1,17 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, extend } from '@react-three/drei'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import * as THREE from 'three'
+
+extend({ TextGeometry })
 
 export default function Avatar({ position = [0, 0, 0], nickname = "Guest" }) {
   const { scene } = useGLTF('/models/Avatar.glb')
+
   return (
     <group position={position}>
       <primitive object={scene} scale={0.3} />
       <mesh position={[0, 2, 0]}>
-        <textGeometry args={[nickname, { size: 0.3, height: 0.05 }]} />
+        <textGeometry args={[nickname, { size: 0.3, height: 0.05, curveSegments: 12 }]} />
         <meshBasicMaterial color="white" />
       </mesh>
     </group>
